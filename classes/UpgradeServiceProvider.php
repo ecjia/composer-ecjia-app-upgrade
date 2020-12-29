4596e9cdcda4b26263f412d568538e6bc7cc6a84
王正东 <wangzhengdong@ecmoban.com>
2020-12-29 18:31:55 +0800

    添加升级后事件监听

--- classes/UpgradeServiceProvider.php
+++ classes/UpgradeServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace Ecjia\App\Upgrade;
 
+use Ecjia\App\Upgrade\Listeners\UpgradeAfterUpdateVersionListener;
+use Ecjia\Component\Version\Events\UpgradeAfterEvent;
+use RC_Event;
 use Royalcms\Component\App\AppParentServiceProvider;
 
 class UpgradeServiceProvider extends  AppParentServiceProvider
@@ -10,13 +13,18 @@ class UpgradeServiceProvider extends  AppParentServiceProvider
     public function boot()
     {
         $this->package('ecjia/app-upgrade');
+
+        $this->bootEvent();
     }
     
     public function register()
     {
         
     }
-    
-    
+
+    protected function bootEvent()
+    {
+        RC_Event::listen(UpgradeAfterEvent::class, UpgradeAfterUpdateVersionListener::class);
+    }
     
 }
